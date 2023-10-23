674e54c1c4ac682ff9d17121bd3dd945bdc035db
이도하 <victoryddh5@naver.com>
2023-10-23 17:24:08 +0900

    FEAT : QueryDSL 테스트 실습

---
+++ src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,176 @@
+package com.example.querydsl;
+
+import com.example.querydsl.member.entity.Member;
+import com.example.querydsl.member.entity.QMember;
+import com.example.querydsl.team.entity.Team;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.example.querydsl.member.entity.QMember.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    @DisplayName("JPQL 테스트")
+    public void startJPQL(){
+
+        // member1 찾기
+
+        String query = "select m " +
+                "from Member m " +
+                "where m.username = :username";
+
+        Member findMember = em.createQuery(query, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    @DisplayName("queryDSL 테스트")
+    public void startQueryDSL(){
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void search(){
+//        Member findMember = queryFactory
+//                .select(member)
+//                .from(member)
+//                .where(
+//                        member.username.eq("member1")
+//                        .and(member.age.eq(10))
+//                )
+//                .fetchOne();
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch(){
+
+        // 리스트
+        List<Member> fetch = queryFactory
+                .select(member)
+                .from(member)
+                .fetch();
+
+        // 한명 조회
+        Member fetchOne = queryFactory
+                .select(member)
+                .from(member)
+                .limit(1)
+                .fetchOne();
+
+        // 리스트 크기
+        Long memberSize= queryFactory
+//                .select(Wildcard.count)
+                .select(member.count())
+                .from(member)
+                .fetchOne();
+    }
+
+    /**
+     * 회원 정렬
+     * 1. 회원 나이 내림차순
+     * 2. 회원 이름 올림차순
+     * 단, 2에서 회원 이름이 없다면 마지막에 출력 (null Last)
+     */
+    @Test
+    public void sort(){
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member)
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member member7 = result.get(2);
+
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+        Assertions.assertThat(member7.getUsername()).isNull();
+
+    }
+
+    @Test
+    public void paging1(){
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        Assertions.assertThat(result.size()).isEqualTo(2);
+
+    }
+
+}
